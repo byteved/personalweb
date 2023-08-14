56f9a3a39e907911536c9387bdf3b1bc42783eb8
Ved Nakum <126437129+byteved@users.noreply.github.com>
2023-08-14 13:40:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function toggleDropdown(id) {
+  const dropdown = document.getElementById(id + "Dropdown");
+  dropdown.classList.toggle("show");
+
+  const otherDropdowns = document.querySelectorAll('.dropdown-content:not(#' + id + 'Dropdown)');
+  otherDropdowns.forEach((otherDropdown) => {
+    otherDropdown.classList.remove('show');
+  });
+}
 // script.js
 window.addEventListener('scroll', function() {
   const header = document.querySelector('.sticky-header');
