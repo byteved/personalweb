60eef0f07a5eea1551bb04e26350bcfb41f78f21
Ved Nakum <126437129+byteved@users.noreply.github.com>
2023-08-13 20:07:57 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <script src="script.js" defer></script>
+  <title>Sticky Header Example</title>
+</head>
+<body>
+ <div id="stripe">
+  <div class="container">
+    <header class="sticky-header">
+      <h1>Ved Nakum, the web dev</h1>
+      <button onclick="" class="mode">About Me</button>
+      <button onclick="" class="mode">My Experience</button>
+      <button onclick="" class="mode">Contact</button>
+    </header>
+    </div>
+    <div class="content">
+      <!-- Your page content goes here -->
+    </div>
+  </div>
+</body>
+</html>
+    
