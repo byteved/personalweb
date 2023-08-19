9525de63f14facf0d7dcea6ca322f137ceeb1d17
Ved Nakum <126437129+byteved@users.noreply.github.com>
2023-08-18 23:32:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-function toggleDropdown(id) {
-  const dropdown = document.getElementById(id + "Dropdown");
-  dropdown.classList.toggle("show");
-
-  const otherDropdowns = document.querySelectorAll('.dropdown-content:not(#' + id + 'Dropdown)');
-  otherDropdowns.forEach((otherDropdown) => {
-    otherDropdown.classList.remove('show');
-  });
-}
 // script.js
 window.addEventListener('scroll', function() {
   const header = document.querySelector('.sticky-header');
@@ -17,3 +8,15 @@ window.addEventListener('scroll', function() {
     header.classList.remove('sticky');
   }
 });
+
+async function loadPage(pageId) {
+  const pageContent = document.getElementById('pageContent');
+
+  try {
+    const response = await fetch(`${pageId}.html`);
+    const content = await response.text();
+    pageContent.innerHTML = content;
+  } catch (error) {
+    pageContent.innerHTML = "<h2>Page Not Found</h2><p>The requested page was not found.</p>";
+  }
+}
